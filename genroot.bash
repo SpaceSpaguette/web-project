e3402758f4f09e442bc7e835830c1c8959ab8f62
spaguette <lukasvachtl01@proton.me>
2025-12-01 13:48:06 +0100

    remodeled file structure

--- genroot.bash
+++ genroot.bash
@@ -1,9 +1,8 @@
-rfolder=("public" "assets" "data" "api" "img")
+rfolder=("public" "assets" "data" "api" "img" "assets/themes" "assets/js" "assets/css")
 for rsfolder in "${rfolder[@]}"; do
   mkdir $rsfolder
 done
-exfolder=("public/index.html" "assets/styles.css assets/themes.css" "data/about.json" "img/favicon.img")
-
+exfolder=("public/index.html" "assets/css/site.css assets/themes/default.css" "data/about.json" "img/favicon.img")
 for extendedf in "${exfolder[@]}"; do
     touch $extendedf
 done
