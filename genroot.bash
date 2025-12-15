6d2b908228db237af74b8c7eefdced5509884d93
spaguette <lukasvachtl01@proton.me>
2025-12-15 09:18:29 +0100

    Reworking it all

--- genroot.bash
+++
@@ -1,8 +0,0 @@
-rfolder=("public" "assets" "data" "api" "img" "assets/themes" "assets/js" "assets/css")
-for rsfolder in "${rfolder[@]}"; do
-  mkdir $rsfolder
-done
-exfolder=("public/index.html" "assets/css/site.css assets/themes/default.css" "data/about.json" "img/favicon.img")
-for extendedf in "${exfolder[@]}"; do
-    touch $extendedf
-done
